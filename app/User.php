4511f0989edf41a02a9e58dc4b5f5a5aec44638a
Administrator <admin@example.com>
2016-05-17 19:39:14 +0000

    Update User.php

--- app/User.php
+++ app/User.php
@@ -23,4 +23,12 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+    
+    /**
+     * Role relationship
+     */
+    public function role()
+    {
+        return $this->belongsTo('App\Role', 'role_number', 'role');
+    }
 }
