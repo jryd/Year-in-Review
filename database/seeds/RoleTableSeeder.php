52585643d284eb8285fd1cf825bd9489e5f3e71d
Administrator <admin@example.com>
2016-05-17 19:28:15 +0000

    Update RoleTableSeeder.php

--- database/seeds/RoleTableSeeder.php
+++ database/seeds/RoleTableSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Role;
 
 class RoleTableSeeder extends Seeder
 {
@@ -11,6 +12,55 @@ class RoleTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        Role::create([
+                'name' => 'CS Day Crew Team Member',
+                'role_number' => 1,
+        ]);
+        
+        Role::create([
+            'name' => 'CS Evening Crew 1 Team Member',
+            'role_number' => 2,
+        ]);
+        
+        Role::create([
+            'name' => 'CS Evening Crew 2 Team Member',
+            'role_number' => 3,
+        ]);
+        
+        Role::create([
+            'name' => 'CS Nightwalker Team Member',
+            'role_number' => 4,
+        ]);
+        
+        Role::create([
+            'name' => 'CS Day Crew Team Leader',
+            'role_number' => 5,
+        ]);
+        
+        Role::create([
+            'name' => 'CS Evening Crew 1 Team Leader',
+            'role_number' => 6,
+        ]);
+        
+        Role::create([
+            'name' => 'CS Evening Crew 2 Team Leader',
+            'role_number' => 7,
+        ]);
+        
+        Role::create([
+            'name' => 'CS Nightwalker Team Leader',
+            'role_number' => 8,
+        ]);
+        
+        Role::create([
+            'name' => 'CS Manager',
+            'role_number' => 9,
+        ]);
+        
+        Role::create([
+            'name' => 'Super Administrator',
+            'role_number' => 10,
+        ]);
+
     }
 }
