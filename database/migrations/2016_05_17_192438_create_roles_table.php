1a99ea64efb2efd3bab9b65aaf855dd4b8b1137c
Administrator <admin@example.com>
2016-05-17 19:26:18 +0000

    Update 2016_05_17_192438_create_roles_table.php

--- database/migrations/2016_05_17_192438_create_roles_table.php
+++ database/migrations/2016_05_17_192438_create_roles_table.php
@@ -12,7 +12,12 @@ class CreateRolesTable extends Migration
      */
     public function up()
     {
-        //
+        Schema::create('roles', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->integer('role_number');
+            $table->timestamps();
+        });
     }
 
     /**
@@ -22,6 +27,6 @@ class CreateRolesTable extends Migration
      */
     public function down()
     {
-        //
+        Schema::drop('roles');
     }
 }
