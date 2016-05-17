a83f576480797824a6f2ff2b36e747bb9d5983df
Administrator <admin@example.com>
2016-05-17 19:34:13 +0000

    Set the Role model up

--- app/Role.php
+++ app/Role.php
@@ -6,5 +6,26 @@ use Illuminate\Database\Eloquent\Model;
 
 class Role extends Model
 {
-    //
+    /**
+	 * The table that the model draws from 
+	 */
+	protected $table = 'roles';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'role_number',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        //
+    ];
 }
