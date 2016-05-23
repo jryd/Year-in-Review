b5f0964fef4ece37694f626b5d7839e16d57cb98
Administrator <admin@example.com>
2016-05-23 09:02:33 +0000

    Update individualstats.blade.php

--- resources/views/stats/individualstats.blade.php
+++ resources/views/stats/individualstats.blade.php
@@ -14,6 +14,7 @@
                   <h3 class="box-title">Title</h3>
     
                   <div class="box-tools pull-right">
+                    <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i></button>
                     <button type="button" class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i></button>
                   </div>
                   <!-- /.box-tools -->
