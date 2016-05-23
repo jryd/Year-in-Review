2bcc9a1bf2abe78ee72ae1699398f79d501180a0
jryd <bannis67@hotmail.com>
2016-05-23 23:27:19 +1200

    Bringing back nav button

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -71,9 +71,9 @@ desired effect
     <!-- Header Navbar -->
     <nav class="navbar navbar-static-top" role="navigation">
       <!-- Sidebar toggle button-->
-      <!-- <a href="#" class="sidebar-toggle" data-toggle="offcanvas" role="button">
+      <a href="#" class="sidebar-toggle" data-toggle="offcanvas" role="button">
         <span class="sr-only">Toggle navigation</span>
-      </a> -->
+      </a>
       <!-- Navbar Right Menu -->
       <div class="navbar-custom-menu">
         <ul class="nav navbar-nav">
