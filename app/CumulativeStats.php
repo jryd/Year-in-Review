527256650fdcb82a3ab27def34ed6916e1a9573b
Administrator <admin@example.com>
2016-05-24 01:11:21 +0000

    Update CumulativeStats.php

--- app/CumulativeStats.php
+++ app/CumulativeStats.php
@@ -6,5 +6,26 @@ use Illuminate\Database\Eloquent\Model;
 
 class CumulativeStats extends Model
 {
-    //
+    /**
+	 * The table that the model draws from 
+	 */
+	protected $table = 'cumulative_stats';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'emails', 'calls', 'development', 'extra_tasks'
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        //
+    ];
 }
