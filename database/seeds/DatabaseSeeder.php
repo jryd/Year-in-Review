72fed6b8617b7788e1bfe715b90a19a2b4f7f45f
Administrator <admin@example.com>
2016-05-24 01:21:12 +0000

    Update DatabaseSeeder.php

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,5 +14,6 @@ class DatabaseSeeder extends Seeder
         $this->call(UsersSeeder::class);
         $this->call(RawStatsSeeder::class);
         $this->call(RoleTableSeeder::class);
+        $this->call(CumulativeStatsSeeder::class);
     }
 }
