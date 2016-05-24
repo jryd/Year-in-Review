826b5470cd42f454c857f3482f59e87871e24ee5
Administrator <admin@example.com>
2016-05-24 03:20:23 +0000

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -29,11 +29,15 @@ class HomeController extends Controller
     {
         $stats = CumulativeStats::find(1);
         $emails = $stats->emails;
+        $cs_email_avg = $stats->cs_email_average;
+        $emails_pp_avg = $stats->emails_per_person_average;
         $calls = $stats->calls;
+        $calls_pp_avg = $stats->calls_per_person_average;
         $development = $stats->development;
         $extra_tasks = $stats->extra_tasks;
         
+        
         Activity::log('Page View: Home');
-        return view('home.home', compact('emails', 'calls', 'development', 'extra_tasks'));
+        return view('home.home', compact('emails', 'cs_email_avg', 'emails_pp_avg', 'calls', 'calls_pp_avg', 'development', 'extra_tasks'));
     }
 }
