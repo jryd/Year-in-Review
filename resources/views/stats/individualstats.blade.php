589d2f238691105e7fa606ad7482511fd2e95d42
Administrator <admin@example.com>
2016-05-25 02:12:11 +0000

    Update individualstats.blade.php

--- resources/views/stats/individualstats.blade.php
+++ resources/views/stats/individualstats.blade.php
@@ -9,22 +9,22 @@
     <!-- Your Page Content Here -->
 	  <div class="row">
 		<div class="col-md-8">
-			<div class="box box-success">
-        <div class="box-header with-border">
-          <h3 class="box-title">Emails and Calls over the year</h3>
-
-          <div class="box-tools pull-right">
-            <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i></button>
-            <button type="button" class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i></button>
-          </div>
-          <!-- /.box-tools -->
-        </div>
-        <!-- /.box-header -->
-        <div class="box-body">
-          <canvas id="emailsandcallsByMonth"></canvas>
-        </div>
-        <!-- /.box-body -->
-      </div>
+			<div class="box box-warning">
+                <div class="box-header with-border">
+                  <h3 class="box-title">Emails and Calls over the year</h3>
+        
+                  <div class="box-tools pull-right">
+                    <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i></button>
+                    <button type="button" class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i></button>
+                  </div>
+                  <!-- /.box-tools -->
+                </div>
+                <!-- /.box-header -->
+                <div class="box-body">
+                  <canvas id="emailsandcallsByMonth"></canvas>
+                </div>
+                <!-- /.box-body -->
+            </div>
 		</div>
 		<div class="col-md-4">
 			<!-- Info Boxes Style 2 -->
