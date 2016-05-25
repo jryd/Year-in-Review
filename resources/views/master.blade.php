627f74ef6a53fa7ebca2a919dc7f1952a36ae882
Administrator <admin@example.com>
2016-05-25 08:12:01 +0000

    Update master.blade.php

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -26,6 +26,18 @@ scratch. This page gets rid of all links and provides the needed markup only.
   
   <!-- Token needed for any ajax calls -->
   <meta name="_token" content="{!! csrf_token() !!}"/>
+  
+  @if (App::environment('production')
+  <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+    
+      ga('create', 'UA-78282566-1', 'auto');
+      ga('send', 'pageview');
+  </script>
+  @endif
 
   <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
   <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
