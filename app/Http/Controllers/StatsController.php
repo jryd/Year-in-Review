a5f74316c56d57ac6d61dcfc224042ec5f3e3aec
Administrator <admin@example.com>
2016-05-25 03:56:34 +0000

    Update StatsController.php

--- app/Http/Controllers/StatsController.php
+++ app/Http/Controllers/StatsController.php
@@ -8,5 +8,20 @@ use App\Http\Requests;
 
 class StatsController extends Controller
 {
-    //
+    public function __construct()
+    {
+        $this->middleware('auth');
+        $this->middleware('setnewpassword');
+    }
+    
+    /**
+     * Show the stats dashboard.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        Activity::log('Page View: My Stats');
+        return view('stats.individualstats');
+    }
 }
