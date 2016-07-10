d26cf514e4f9afbf0cf57b392273dee004c1c9c4
jryd <bannis67@hotmail.com>
2016-07-10 22:36:32 +1200

    Updating avatar to user image

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -75,14 +75,14 @@ scratch. This page gets rid of all links and provides the needed markup only.
             <!-- Menu Toggle Button -->
             <a href="#" class="dropdown-toggle" data-toggle="dropdown">
               <!-- The user image in the navbar-->
-              <img src="dist/img/user2-160x160.jpg" class="user-image" alt="User Image">
+              <img src="{{ asset('img/' . Auth::user()->id . '.jpg') }}" class="user-image" alt="User Image">
               <!-- hidden-xs hides the username on small devices so only the image appears. -->
               <span class="hidden-xs">{{ Auth::user()->first_name }}</span>
             </a>
             <ul class="dropdown-menu">
               <!-- The user image in the menu -->
               <li class="user-header">
-                <img src="dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
+                <img src="{{ asset('img/' . Auth::user()->id . '.jpg') }}" class="img-circle" alt="User Image">
 
                 <p>
                   {{ Auth::user()->first_name }} {{ Auth::user()->last_name }}
