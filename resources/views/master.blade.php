d83550324703cf67abb6cd31bc3b0e50ad84982d
Administrator <admin@example.com>
2016-07-11 08:59:08 +0000

    Update master.blade.php

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -35,7 +35,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
       m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
       })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
     
-      ga('create', 'UA-78282566-1', 'auto');
+      ga('create', 'UA-78282566-2', 'auto');
       ga('send', 'pageview');
   </script>
   @endif
